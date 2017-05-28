14277734a4830b12aa9610f707fd1b5ebcdfd2c0
Rachaelf2017 <rfeuerborn2017@kellogg.northwestern.edu>
2017-05-28 18:33:27 -0500

    finished #2

--- numbers/numbers.js
+++ numbers/numbers.js
@@ -5,10 +5,16 @@ Keep a limit to how far the program will go.*/
 $(function() {
 
 
-  $("#button").click(function(){
-      var n = $("#decimal").val();
+  $("#button1").click(function(){
+      var n = $("#decimal1").val();
       n = parseInt(n)
-      $(".result").html("Answer: " + Math.PI.toFixed(n))
+      $(".result1").html("Answer: " + Math.PI.toFixed(n))
+  })
+
+  $("#button2").click(function(){
+      var m = $("#decimal2").val();
+      m = parseInt(m)
+      $(".result2").html("Answer: " + Math.E.toFixed(m))
   })
 
 
