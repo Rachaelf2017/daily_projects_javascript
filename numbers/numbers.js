48dab89f624b26893dd79e753ab091c31455f9bc
Rachaelf2017 <rfeuerborn2017@kellogg.northwestern.edu>
2017-05-29 09:50:11 -0500

    finished #3

--- numbers/numbers.js
+++ numbers/numbers.js
@@ -1,4 +1,5 @@
-/*1. Find PI to the Nth Digit -
+
+/*1. Find PI to the Nth Digit :
 Enter a number and have the program generate PI up to that many decimal places.
 Keep a limit to how far the program will go.*/
 
@@ -7,14 +8,33 @@ $(function() {
 
   $("#button1").click(function(){
       var n = $("#decimal1").val();
-      n = parseInt(n)
+      var n = parseInt(n)
       $(".result1").html("Answer: " + Math.PI.toFixed(n))
   })
 
+  /*2. Find e to the Nth Digit:
+    Just like the previous problem, but with e instead of PI.
+    Enter a number and have the program generate e up to that many decimal places.
+    Keep a limit to how far the program will go. */
+
   $("#button2").click(function(){
-      var m = $("#decimal2").val();
-      m = parseInt(m)
-      $(".result2").html("Answer: " + Math.E.toFixed(m))
+      var n = $("#decimal2").val();
+      var n = parseInt(n)
+      $(".result2").html("Answer: " + Math.E.toFixed(n))
+  })
+
+
+function fSequence(n){
+var sequence = [0,1]
+  for (i = 0; i < n; i++)
+    sequence.push((sequence[i] + sequence[i+1]))
+  return sequence;
+}
+
+  $("#button3").click(function(){
+      var n = $("#f-sequence").val();
+      var n = parseInt(n)
+      $(".result3").html("Answer: " + fSequence(n-2))
   })
 
 
